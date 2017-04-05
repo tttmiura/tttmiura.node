1553b9f80ea2b55464f3745a14a6a43cc4a0536b
namakemonoggg <namakemonoggg@gmail.com>
2017-04-05 02:49:02 -0400

    test

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.on('ready', function() {
     height: 600
   });
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/main.html');
 
 
 
